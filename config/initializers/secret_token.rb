50b37b63aa1aa304cae34cc1b916cdc3e5faf79a
Corey Murphy <corey.p.murphy@gmail.com>
2011-06-14 22:06:32 -0500

    created initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duffers::Application.config.secret_token = 'baf7591b7fc2e3113e296b4d187edda8c331c9859fb7f2f2a12990fee64d3f02de8e4c62aa0ccde2b4ef68ad9a47e1b9e101d252757d922754449382b35c4de0'
